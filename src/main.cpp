871a1952eecac90307591b170b66db7bdb3fdcb6
Ermanno Righini <ermannorighini@gmail.com>
2022-03-22 03:41:02 +0100

    integration with nanobench

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "graphgen/randomgraphs.hpp"
 #define DOCTEST_CONFIG_DISABLE
 #include <doctest.h>
+#define ANKERL_NANOBENCH_IMPLEMENT
+#include <nanobench.h>
 
 #include <iostream>
 
@@ -15,9 +17,23 @@ int main() {
 
   Random rnd(31);
   Edges edges;
-  randomGraph(rnd, N, M, 1.0, edges);
+  float cost1, cost2;
 
-  float cost = filterKruskal(edges, N);
-  std::cout << "Result: " << cost << std::endl;
+  ankerl::nanobench::Bench().minEpochIterations(100).run(
+      "Graph generation", [&] { randomGraph(rnd, N, M, 1.0, edges); });
+
+  std::cout << edges.size() << std::endl;
+
+  ankerl::nanobench::Bench().minEpochIterations(5).run("Kruskal", [&] {
+    Edges edgesCopy = edges;
+    cost1 = kruskal(edgesCopy, N);
+  });
+
+  ankerl::nanobench::Bench().minEpochIterations(100).run("FilterKruskal", [&] {
+    Edges edgesCopy = edges;
+    cost2 = filterKruskal(edgesCopy, N);
+  });
+
+  std::cout << "Result: " << cost1 << " " << cost2 << std::endl;
   return 0;
 }
