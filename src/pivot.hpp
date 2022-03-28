b63dc8311182eb8273000e0a9765a84aaae16ade
Ermanno Righini <ermannorighini@gmail.com>
2022-03-28 19:25:52 +0200

    Now pivot helper functions move pivots to the start of the edge list to improve performance and code readability

--- src/pivot.hpp
+++ src/pivot.hpp
@@ -7,24 +7,27 @@
 #include "utils/graph.hpp"
 #include "utils/random.hpp"
 
-// picks the pivot randomply from the edge list
-static inline EdgeIt pickRandomPivot(EdgeIt first, EdgeIt last) {
+// picks the pivot randomply from the edge list and moves it to the start of the
+// list
+static inline EdgeIt pickRandomPivot(EdgeIt &first, EdgeIt &last) {
   static Random rnd(31);
-  return first + rnd.getULong(last - first);
+  EdgeIt pivotPos = first + rnd.getULong(last - first);
+  std::swap(*pivotPos, *first);
+  return first++;
 }
 
-// picks sqrt(k) samples and returns the new start iterator of the array
+// picks sqrt(k) samples and returns the start iterator of the samples
 // (the prefix is filled with the samples)
-static inline EdgeIt pickRandomSampleRootK(EdgeIt first, EdgeIt last) {
+static inline EdgeIt pickRandomSampleRootK(EdgeIt &first, EdgeIt &last) {
   static Random rnd(31);
   int nSamples = std::max(int(std::sqrt(last - first)), 1);
 
   EdgeIt firstSample = first;
   for (int i = 0; i < nSamples; i++) {
     EdgeIt picked = first + rnd.getULong(last - first);
-    std::iter_swap(first++, picked);
+    std::swap(*(first++), *picked);
   }
 
   std::sort(firstSample, first);
-  return first;
+  return firstSample;
 }
