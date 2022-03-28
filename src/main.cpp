0e0c87eb39690b4eebf277cb6eac1fc5c1af48c1
Ermanno Righini <ermannorighini@gmail.com>
2022-03-28 19:26:57 +0200

    Implemented algorithm to find the optimal pivoting strategy for a specific problem instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,35 @@
 #include <chrono>
 
-#include "graphgen/randomgraphs.hpp"
 #define DOCTEST_CONFIG_DISABLE
 #include <doctest.h>
+
 #define ANKERL_NANOBENCH_IMPLEMENT
 #include <nanobench.h>
 
 #include <iostream>
 
 #include "filterkruskal.hpp"
+#include "graphgen/randomgraphs.hpp"
+#include "optimalpivot.hpp"
 #include "partialmst.hpp"
 #include "unionfind.hpp"
 
 int main() {
-  std::cout << "hello" << std::endl;
+  Random rnd(23);
 
-  int N = 1000;
-  int M = 1000000;
+  int N = 10;
+  int M = 20;
+  Edges edges;
+  randomGraphOneLong(rnd, N, M, 1.0, edges);
+  Edges pivots = findBestPivots(edges, N);
+  std::cout << "OK" << std::endl;
+  std::cout << pivots << std::endl;
 
-  Random rnd(31);
+  return 0;
 
   ankerl::nanobench::Bench bench;
+  // solve with the extracted pivots
+  // Edges mst = seededFilterKruskal(edges, N, pivots);
 
   for (int M = 10; M < 1000000; M *= 1.5) {
     int N = std::max((int)std::sqrt(M), 2);
@@ -29,26 +38,35 @@ int main() {
     Edges edges;
     randomGraph(rnd, N, M, 1.0, edges);
 
+    bench.complexityN(M).minEpochIterations(10).run("FindPivot", [&] {
+
+    });
+
     bench.complexityN(M).minEpochIterations(100).run("FilterKruskal", [&] {
       Edges edgesCopy = edges;
-      float cost = filterKruskal(edgesCopy, N);
-      ankerl::nanobench::doNotOptimizeAway(cost);
+      Edges mst = filterKruskal(edgesCopy, N);
+      ankerl::nanobench::doNotOptimizeAway(mst);
     });
 
     bench.complexityN(M).minEpochIterations(100).run("Kruskal", [&] {
       Edges edgesCopy = edges;
-      float cost = kruskal(edgesCopy, N);
-      ankerl::nanobench::doNotOptimizeAway(cost);
+      Edges mst = kruskal(edgesCopy, N);
+      ankerl::nanobench::doNotOptimizeAway(mst);
     });
 
-    bench.complexityN(M)
-        .minEpochIterations(1)
-        .maxEpochTime(std::chrono::nanoseconds(1))
-        .run("ImprovedKruskal", [&] {
-          Edges edgesCopy = edges;
-          float cost = improvedKruskal(edgesCopy, N);
-          ankerl::nanobench::doNotOptimizeAway(cost);
-        });
+    // std::cout << N << " " << edges.size() << std::endl;
+    // std::cout << edges << std::endl;
+    // std::cout << pivots << std::endl;
+    // std::cout << "OK" << std::endl;
+    // return 0;
+
+    // Edges pivots = findBestPivots(edges, N);
+    // bench.complexityN(M).minEpochIterations(100).run("KruskalSeeded", [&] {
+    //   Edges edgesCopy = edges;
+    //   Edges pivotsCopy = pivots;
+    //   Edges mst = filterKruskalSeeded(edgesCopy, N, pivotsCopy);
+    //   ankerl::nanobench::doNotOptimizeAway(mst);
+    // });
   }
 
   return 0;
---
+++ src/optimalpivot.hpp
@@ -0,0 +1,209 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <tuple>
+#include <utility>
+
+#include "filterkruskal.hpp"
+#include "kruskal.hpp"
+#include "partition.hpp"
+#include "unionfind.hpp"
+#include "utils/graph.hpp"
+
+struct BestPivotFinder {
+  Edges edges;
+  int N;
+  Edges mst;
+  std::vector<int> filteredBy, lastEdge;
+  std::vector<std::vector<int>> count;
+
+  struct Result {
+    u64 cost;
+    int pivot;
+  };
+
+  typedef std::pair<int, int> Pii;
+  typedef std::map<Pii, Result> Mpp;
+  Mpp dp;
+
+  BestPivotFinder(const Edges &edges, int N) : edges(edges), N(N) {}
+
+  std::vector<Edge> getBestPivots() {
+    customKruskal();
+    // std::cout << edges << std::endl;
+    std::cout << mst << std::endl;
+    // std::cout << lastEdge << std::endl;
+    // std::cout << filteredBy << std::endl;
+
+    calcCount();
+
+    // for (int i = 0; i <= mst.size(); i++) {
+    //   for (int j = 0; j + i <= mst.size(); j++) {
+    //     auto [cost, pivot] = solve(j, j + i);
+    //     std::cout << cost << ' ';
+    //   }
+    //   std::cout << std::endl;
+    // }
+    // std::cout << std::endl;
+
+    // auto [cost, pivot] = solve(0, mst.size());
+    // std::cout << pivot << " " << cost << std::endl;
+
+    // for (int i = 0; i < mst.size(); i++) {
+    //   std::cout << calcCost(0, mst.size(), i) << " ";
+    // }
+    // std::cout << std::endl;
+
+    std::vector<Edge> pivots;
+    getPivotsRec(0, mst.size(), pivots);
+
+    return pivots;
+  }
+
+  void customKruskal() {
+    std::sort(edges.begin(), edges.end());
+    DisjointSet set(N);
+    mst.clear();
+    lastEdge = std::vector<int>(edges.size());
+    filteredBy = std::vector<int>(edges.size());
+    for (int i = 0; i < edges.size(); i++) {
+      if (addEdgeToMst(set, edges[i], mst)) {
+        int mstEdgeId = mst.size() - 1;
+        filteredBy[i] = mstEdgeId;
+
+        for (int j = i; j < edges.size(); j++) {
+          const Edge &e = edges[j];
+          lastEdge[j] = mstEdgeId + 1;
+          if (filteredBy[j] == 0 && set.compare(e.a, e.b)) {
+            filteredBy[j] = mstEdgeId;
+          }
+        }
+      }
+    }
+  }
+
+  void calcCount() {
+    count = std::vector<std::vector<int>>(mst.size() + 1);
+    for (int i = 0; i < count.size(); i++) {
+      auto &cc = count[i] = std::vector<int>(mst.size() + 1, 0);
+    }
+
+    // count edges inside each bucket
+    for (int j = 0; j < filteredBy.size(); j++) {
+      count[filteredBy[j]][lastEdge[j]] += 1;
+    }
+
+    for (int i = count.size() - 2; i >= 0; i--) {
+      // horizontal prefix sum
+      for (int j = 1; j < count[0].size(); j++) {
+        count[i][j] += count[i][j - 1];
+      }
+
+      // vertical postfix sum
+      for (int j = 1; j < count[0].size(); j++) {
+        count[i][j] += count[i + 1][j];
+      }
+    }
+
+    // for (int i = 0; i < count.size(); i++) {
+    //   std::cout << i << ": " << count[i] << std::endl;
+    // }
+  }
+
+  u64 calcCost(int l, int r, int pivot, int depth = 0) {
+    assert(l >= 0);
+    assert(r <= mst.size());
+    assert(l <= r);
+    assert(l <= pivot);
+    assert(pivot < r);
+
+    float cost = count[l][r] - count[l][l];         // partition cost
+    cost += solve(l, pivot, depth + 1).cost;        // left solve
+    if (pivot < mst.size() - 1) {                   // is last pivot?
+      cost += count[l][r] - count[l][pivot] - 1;    // right filter
+      cost += solve(pivot + 1, r, depth + 1).cost;  // right solve
+    }
+    return cost;
+  }
+
+  Result solve(int l, int r, int depth = 0) {
+    assert(l >= 0);
+    assert(r <= mst.size());
+    assert(l <= r);
+
+    // base case
+    if (r - l == 0) {
+      return {0, -1};
+    }
+
+    // recursive case
+    if (dp.count({l, r}) == 0) {
+      Result best = {0, -1};
+      for (int pivot = l; pivot < r; pivot++) {
+        u64 cost = calcCost(l, r, pivot, depth);
+        if (best.cost == 0 || cost < best.cost) {
+          best = {cost, pivot};
+        }
+      }
+      assert(best.cost > 0);  // impossible
+      dp[{l, r}] = best;
+    }
+    // std::cout << std::string(depth, ' ') << dp[{l, r}].first << std::endl;
+    return dp[{l, r}];
+  }
+
+  void getPivotsRec(int l, int r, Edges &pivots) {
+    assert(l >= 0);
+    assert(r <= mst.size());
+    assert(l <= r);
+    int p = solve(l, r).pivot;
+    if (p == -1) return;
+    assert(p >= l);
+    assert(p < r);
+
+    pivots.push_back(mst[p]);
+    getPivotsRec(l, p, pivots);
+    getPivotsRec(p + 1, r, pivots);
+  }
+};
+
+static inline std::vector<Edge> findBestPivots(const Edges &edges, int N) {
+  BestPivotFinder finder(edges, N);
+  auto pivots = finder.getBestPivots();
+  return pivots;
+}
+
+static inline void filterKruskalSeeded(DisjointSet &set, Edges &edges,
+                                       int first, int last, int N, Edges &mst,
+                                       EdgeIt &nextPivot) {
+  u64 M = last - first;
+  if (M == 0) return;
+  if (M < 100)
+    return kruskal(set, edges.begin() + first, edges.begin() + last, N, true,
+                   mst);
+
+  Edge pivot = *nextPivot++;
+  int mid = partitionSkipPivot(edges, first, last, pivot);
+
+  filterKruskalSeeded(set, edges, first, mid, N, mst, nextPivot);
+
+  if (mst.size() < N - 1) addEdgeToMst(set, pivot, mst);
+  if (mst.size() < N - 1) {
+    last = filterAll(set, edges.begin() + mid, edges.begin() + last) -
+           edges.begin();
+    filterKruskalSeeded(set, edges, mid, last, N, mst, nextPivot);
+  }
+}
+
+static inline Edges filterKruskalSeeded(Edges &edges, int N, Edges &pivots) {
+  // ciao
+  DisjointSet set(N);
+  EdgeIt nextPivot = pivots.begin();
+  Edges mst;
+  filterKruskalSeeded(set, edges, 0, edges.size(), N, mst, nextPivot);
+  return mst;
+}
